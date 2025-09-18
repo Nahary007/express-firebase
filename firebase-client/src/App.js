78e9531e1b51d37d6ba2a865411f82a1c05644ba
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-09-18 22:19:55 +0300

    feat/ create table and fetch data

--- firebase-client/src/App.js
+++ firebase-client/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState} from "react";
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:4004/getUsers')
+    .then(response => {
+      console.log(response.data)
+      setUsers(response.data);
+    })
+    .catch(error => {
+      console.error('Erreur : ', error);
+    }) ;
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Liste des utilisateurs</h1>
+      <table border="1" class="table table-striped table-hover ">
+        <tr>
+          <td><strong>Nom</strong></td>
+          <td><strong>Email</strong></td>
+          <td><strong>Age</strong></td>
+        </tr>
+        {
+          users.map(user => (
+            <tr key={user.id}>
+              <td> {user.nom} </td>
+              <td> {user.email} </td>
+              <td> {user.age} </td>
+            </tr>
+          ))
+        }
+      </table>
     </div>
   );
 }
